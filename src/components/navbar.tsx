72c46b02a12a0b3be023d3d7f483cb3a14162ec9
Faizan <faizanalam980@gmail.com>
2024-10-25 19:53:38 +0530

    added singin and signout button with user image after login

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import { ModeToggle } from "./mode-toggle";
 import { Button } from "@/components/ui/button";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { LogOut } from "lucide-react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 
 export function Navbar() {
+  const { data: session } = useSession();
+
   return (
     <header className="sticky top-0 z-50 w-full border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="w-full flex items-center justify-end p-3 gap-x-2">
@@ -10,7 +17,23 @@ export function Navbar() {
           <GitHubLogoIcon />
         </Button>
         <ModeToggle />
-        <Button>Login</Button>
+        {session && session.user ? (
+          <>
+            {/* <p className="text-sky-600">{session.user.name}</p> */}
+            <Image
+              src={session.user.image ?? ""}
+              alt={session.user.name ?? ""}
+              className=" rounded-full"
+              width={32}
+              height={32}
+            />
+            <Button variant="ghost" onClick={() => signOut()}>
+              Logout <LogOut />
+            </Button>
+          </>
+        ) : (
+          <Button onClick={() => signIn()}>Sign In</Button>
+        )}
       </div>
     </header>
   );
