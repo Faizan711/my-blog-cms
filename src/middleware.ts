103fbbc3f22025e8c70cd9e2b49ef51fece09138
Faizan <faizanalam980@gmail.com>
2024-10-26 20:51:26 +0530

    middlware to protect admin routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,25 @@
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export default withAuth({
-  callbacks: {
-    authorized: async ({ req, token }) => {
-      if (req.nextUrl.pathname.startsWith("/admin"))
-        return token?.role === "admin";
-      return !!token;
-    },
-  },
+// export default withAuth({
+//   callbacks: {
+//     authorized: async ({ req, token }) => {
+//       if (req.nextUrl.pathname.startsWith("/admin"))
+//         return token?.role === "admin";
+//       return !!token;
+//     },
+//   },
+//   pages: {
+//     signIn: "/login",
+//   },
+// });
+
+export default withAuth((req) => {
+  if (req.nextUrl.pathname.startsWith("/admin")) {
+    if (req.nextauth.token?.role !== "admin") {
+      return NextResponse.redirect(new URL("/unauthorized", req.url));
+    }
+  }
 });
+
 export const config = { matcher: ["/admin:path*"] };
